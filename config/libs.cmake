e566f7e53182b177ac1201e2796959a2e443bd31
Viacheslav <slava.k@ks2corp.com>
2023-12-03 10:33:46 +0300

    added ring-buffer

--- config/libs.cmake
+++ config/libs.cmake
@@ -1,6 +1,13 @@
 FetchContent_GetProperties(lwgps)
+FetchContent_GetProperties(ring-buffer)
 
 message("{lwgps_SOURCE_DIR}=" ${lwgps_SOURCE_DIR})
+message("{ring-buffer_SOURCE_DIR}=" ${ring-buffer_SOURCE_DIR})
+
 file(GLOB LWGPS_LIB_SOURCES
     ${lwgps_SOURCE_DIR}/lwgps/src/lwgps/lwgps.c
+)
+
+file(GLOB RING_BUFFER_LIB_SOURCES
+    ${ring-buffer_SOURCE_DIR}/ringbuffer.c
 )

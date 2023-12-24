5bca68a31da04f09036ade2caf3f46bf4f2a56b9
Viacheslav <slava.k@ks2corp.com>
2023-12-24 16:34:02 +0300

    added irq handler

--- hw_test/ws407/lis3dsh/application.cpp
+++ hw_test/ws407/lis3dsh/application.cpp
@@ -14,12 +14,20 @@
 #include "app/utils/delay.h"
 #include "app/io/gpio/gpio.h"
 #include "libs/lis3dsh/lis3dsh.h"
+#include "main.h"
 //>>---------------------- Log control
 #define LOG_MODULE_NAME acctst
 #define LOG_MODULE_LEVEL (3)
 #include "app/debug/log_libs.h"
 //<<----------------------
 
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (GPIO_Pin == ACC_INT_1_Pin)
+    {
+        LOG_INFO("acc irq");
+    }
+}
 /**
  * @brief Test application
  *
@@ -29,6 +37,19 @@ void application(void)
     uint8_t id = LIS3DSH_ReadID();
     LOG_INFO("id: 0x%02x", id);
 
+    LIS3DSH_DataRateCmd(LIS3DSH_DATARATE_50);
+
+    LIS3DSH_InterruptConfigTypeDef LIS3DSH_InterruptStruct;
+    /* Set LIS3DSH Interrupt configuration */
+    LIS3DSH_InterruptStruct.Interrupt_Selection_Enable = LIS3DSH_INTERRUPT_1_ENABLE;
+    LIS3DSH_InterruptStruct.Interrupt_Request = LIS3DSH_INTERRUPT_REQUEST_PULSED;
+    LIS3DSH_InterruptStruct.Interrupt_Signal = LIS3DSH_INTERRUPT_SIGNAL_HIGH;
+    LIS3DSH_InterruptStruct.State_Machine1_Enable = LIS3DSH_SM_ENABLE;
+    LIS3DSH_InterruptStruct.State_Machine2_Enable = LIS3DSH_SM_ENABLE;
+    LIS3DSH_InterruptStruct.State_Machine1_Interrupt = LIS3DSH_SM_INT1;
+    LIS3DSH_InterruptStruct.State_Machine2_Interrupt = LIS3DSH_SM_INT1;
+
+    LIS3DSH_InterruptConfig(&LIS3DSH_InterruptStruct);
     bool red = true;
     while (true)
     {

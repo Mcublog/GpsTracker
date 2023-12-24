50eb4a4dd0014483dd6eb8d4a9c5221c2d00eb7d
Viacheslav <slava.k@ks2corp.com>
2023-12-24 15:57:10 +0300

    cleanup

--- app/io/serial/types.h
+++ app/io/serial/types.h
@@ -10,32 +10,32 @@ extern "C"
 {
 #endif
 
-typedef struct
-{
-    uint32_t size;
-    uint8_t data[];
-} ios_message_t;
+    typedef struct
+    {
+        uint32_t size;
+        uint8_t data[];
+    } ios_message_t;
 
-typedef struct
-{
-    uint8_t *data;
-    uint32_t limit;
-} ios_buf_t;
+    typedef struct
+    {
+        uint8_t *data;
+        uint32_t limit;
+    } ios_buf_t;
 
-typedef struct
-{
-    uint8_t *data;
-    uint32_t size;
-} ios_chunk_t;
+    typedef struct
+    {
+        uint8_t *data;
+        uint32_t size;
+    } ios_chunk_t;
 
-typedef uint32_t (*ios_irq_handler_t)(ios_chunk_t *chunk);
+    typedef uint32_t (*ios_irq_handler_t)(ios_chunk_t *chunk);
 
-typedef struct
-{
-    ios_buf_t in;
-    ios_buf_t out;
-    ios_irq_handler_t irq_handler;
-} ios_ctl_t;
+    typedef struct
+    {
+        ios_buf_t in;
+        ios_buf_t out;
+        ios_irq_handler_t irq_handler;
+    } ios_ctl_t;
 
 #ifdef __cplusplus
 }

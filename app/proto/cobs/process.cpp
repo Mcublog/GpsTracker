a1394cb792b00d5f98e3cac372f313ef959f0b7c
Viacheslav <slava.k@ks2corp.com>
2023-12-30 12:53:37 +0300

    added time printing

--- app/proto/cobs/process.cpp
+++ app/proto/cobs/process.cpp
@@ -67,6 +67,7 @@ bool Cobs::process(void)
         {
             lwgps_t *gnss = m_gnssp->read_message();
             LOG_INFO("Valid status: %d: %s", gnss->is_valid, tu_print_current_time_only());
+            LOG_INFO("Time: %02d:%02d:%02d", gnss->hours, gnss->minutes, gnss->seconds);
             LOG_INFO("Latitude: %f degrees", gnss->latitude);
             LOG_INFO("Longitude: %f degrees", gnss->longitude);
             LOG_INFO("Altitude: %f meters", gnss->altitude);
